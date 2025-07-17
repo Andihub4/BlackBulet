c7f43c095af833eba61c0675e69a6c58075a78a1
Andinet <Andinetatx@gmail.com>
2025-07-17 06:53:13 -0700

    Add files via upload

---
+++ Andi.js
@@ -0,0 +1,8 @@
+function BB() {
+  
+window.alert('Then click "Contact boss" button below to chat with the boss group ')
+
+}
+function BB0() {
+  window.alert("So you have to close out.")
+}
---
+++ Andi.sql
@@ -0,0 +1,8 @@
+create table (name varchar(16),
+Fname varchar(16),
+age varchar(2),
+Id primary key)
+
+insert into student (name,Fname,age,Id)
+values (@name , @Fname, @age ,@Id,@mark)
+
